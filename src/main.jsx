dde0e1ddfa1b3e14a38985ed40c9115359133d12
zannettidigitalassets-spec <zannettidigitalassets@gmail.com>
2026-06-15 22:04:32 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Landing from './Landing.jsx'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path.startsWith('/app') ? <App /> : <Landing />}
   </React.StrictMode>
 )
